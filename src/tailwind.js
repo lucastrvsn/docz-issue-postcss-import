90507dd5b602546d20db14135e12e6f1d1725358
Lucas Trevisan <lucastrvsn@gmail.com>
2020-12-16 10:21:29 -0300

    Update tailwind.js

--- src/tailwind.js
+++ src/tailwind.js
@@ -1,6 +1,6 @@
 const plugin = require('tailwindcss/plugin')(
   ({ theme, addBase, addComponents }) => {
-    // IF YOU EDIT THIS, IT WILL NOT BE RELOADED
+    // IF YOU EDIT THIS REQUIRED FILE, IT WILL NOT BE RELOADED BUT GATSBY WILL TRIGGER RELOADING
     addComponents(require('./components.js')(theme));
 
     // IF YOU EDIT THIS, IT WILL BE RELOADED
